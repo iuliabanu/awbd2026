eb66baa9dec6e6814723d9383fc7b1869dac49a6
Iulia Banu <th_iulia84@yahoo.com>
2026-03-14 19:32:45 +0200

    Lab 2 final

---
+++ lab2_final/src/main/java/com/awbd/lab2/domain/Category.java
@@ -0,0 +1,26 @@
+package com.awbd.lab2.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    @JoinTable(name = "product_category",
+            joinColumns = @JoinColumn(name = "category_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id", referencedColumnName = "id"))
+    @ManyToMany
+    private List<Product> products;
+
+}
